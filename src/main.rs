f8237d996c77e186f9f6259497b46bf36d44460b
anwitars <maksa22242@gmail.com>
2025-03-19 22:41:55 +0100

    refactor: use unified Cli and AppArgs

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,23 @@
 use std::net::SocketAddr;
 
 use clap::Parser;
-use tiny_trails::{app, utils::env::TT_ENV_PREFIX, value_from_env};
+use constcat::concat;
+use tiny_trails::{app, utils::env::TT_ENV_PREFIX};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Debug, clap::Parser)]
-struct Cli {
-    #[clap(long)]
-    pub host: Option<String>,
-
-    #[clap(short, long)]
-    pub port: Option<u16>,
-
-    #[clap(short, long)]
-    pub database: Option<String>,
-}
-
-#[derive(Debug, Clone)]
 struct AppArgs {
+    #[clap(long, default_value = "0.0.0.0", env = concat!(TT_ENV_PREFIX, "HOST"))]
     pub host: String,
+
+    #[clap(short, long, default_value = "3000", env = concat!(TT_ENV_PREFIX, "PORT"))]
     pub port: u16,
+
+    #[clap(short, long, env = concat!(TT_ENV_PREFIX, "DATABASE"))]
     pub database: String,
 }
 
 impl AppArgs {
-    pub fn from_env_and_cli(cli: &Cli) -> Self {
-        let env_host = value_from_env!("HOST");
-        let env_port = value_from_env!("PORT", u16);
-        let env_database = value_from_env!("DATABASE");
-
-        let host = cli
-            .host
-            .clone()
-            .unwrap_or(env_host.unwrap_or("0.0.0.0".to_string()));
-        let port = cli.port.unwrap_or(env_port.unwrap_or(3000));
-        let database = cli.database.clone().unwrap_or_else(|| {
-            env_database.unwrap_or_else(|| panic!("Database must be set via environment or CLI"))
-        });
-
-        Self {
-            host,
-            port,
-            database,
-        }
-    }
-
     pub fn listen_address(&self) -> String {
         format!("{}:{}", self.host, self.port)
     }
@@ -61,8 +34,7 @@ async fn main() {
         )
         .init();
 
-    let cli = Cli::parse();
-    let app_args = AppArgs::from_env_and_cli(&cli);
+    let app_args = AppArgs::parse();
     log::debug!("App args: {:?}", app_args);
 
     let pool = sqlx::PgPool::connect(&app_args.database).await.unwrap();
