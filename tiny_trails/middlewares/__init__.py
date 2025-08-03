df74a3d2ed25d0964e52af6942ee7ba4ba960d72
anwitars <maksa22242@gmail.com>
2025-08-03 21:11:14 +0200

    fix: api: cors middleware

--- tiny_trails/middlewares/__init__.py
+++ tiny_trails/middlewares/__init__.py
@@ -1,8 +1,19 @@
 from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from tiny_trails.database import Database
 from tiny_trails.middlewares.context import ContextMiddleware
 
 
 def create_middlewares(db: Database) -> list[Middleware]:
-    return [Middleware(ContextMiddleware, db=db)]
+    return [
+        # as it is an open API, we allow all origins, methods, and headers
+        Middleware(
+            CORSMiddleware,
+            allow_origins=["*"],  # Allow all origins for CORS
+            allow_credentials=True,
+            allow_methods=["*"],  # Allow all methods
+            allow_headers=["*"],
+        ),  # Allow all headers
+        Middleware(ContextMiddleware, db=db),
+    ]
