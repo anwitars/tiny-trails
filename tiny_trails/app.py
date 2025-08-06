6ea964e1dd02fd8374b5280d33c6635e79ddaa34
anwitars <maksa22242@gmail.com>
2025-08-06 18:23:26 +0200

    env: generate openapi docs with correct version of the application

--- tiny_trails/app.py
+++ tiny_trails/app.py
@@ -5,13 +5,19 @@ from tiny_trails.middlewares import create_middlewares
 from tiny_trails.routing import assign_routes
 
 
+def _get_version() -> str:
+    from importlib.metadata import version
+
+    return version("tiny-trails")
+
+
 def create_app(*, db_url: str) -> FastAPI:
     """
     Create a FastAPI application instance with middlewares and routes.
     """
 
     db = PostgresDatabase(url=db_url)
-    app = FastAPI(middleware=create_middlewares(db))
+    app = FastAPI(middleware=create_middlewares(db), version=_get_version())
     assign_routes(app)
 
     return app
@@ -23,6 +29,6 @@ def create_app_for_docs() -> FastAPI:
     This instance does not include middlewares or database connections.
     """
 
-    app = FastAPI()
+    app = FastAPI(version=_get_version())
     assign_routes(app)
     return app
